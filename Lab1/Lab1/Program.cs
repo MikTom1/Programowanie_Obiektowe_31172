a1f70c31dcecb31473d46c515d91880cb2adf6ad
MikTom1 <mtomaszczyk@edu.cdv.pl>
2025-10-11 10:33:54 +0200

    Zadanie Lab

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Zadanie 1 
+/*string password = "admin123";
+string userpass;
 
-Console.WriteLine("Hello, World!");
+do
+{
+    Console.WriteLine("Podaj haslo");
+    userpass=Console.ReadLine();
+    if (userpass != password)
+    {
+        Console.WriteLine("Bledne haslo, sprobuj ponownie.");
+    }
+    
+    
+    
+} while (userpass!=password);
+Console.WriteLine("Witaj");*/
+
+// Zadanie 2 
+
+/*var liczba1=0;
+
+do
+{
+    Console.WriteLine("Podaj liczbe wieksza od 0:");
+    var liczba=Console.ReadLine();
+    
+    liczba1 =Convert.ToInt32(liczba);
+    if (liczba1 <= 0)
+    {
+        Console.WriteLine("Liczba mniejsza lub rowna 0. Sprobuj ponownie");
+    }
+} while (liczba1 <=0);
+Console.WriteLine("koniec");
+
+
+// Zadanie3 
+/*
+string[] cities = { "Lizbona","Madryt","Paryż","Poznan","Warszawa" };
+
+foreach (var city in cities)
+{
+    Console.WriteLine(city);
+}
+*/
