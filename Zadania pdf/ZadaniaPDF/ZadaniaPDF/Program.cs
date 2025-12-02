6632487b9ec449f21ec13f8ed60968879c101eb3
Mikolaj <mtomaszczyk@edu.cdv.pl>
2025-12-02 19:46:20 +0100

    Zadania PDF

---
+++ Zadania pdf/ZadaniaPDF/ZadaniaPDF/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Zadanie 5
+/*class KontoBankowe
+{
+    private double saldo;
+
+    public void Wplata(double kwota)
+    {
+        saldo += kwota;
+    }
+
+    public double PobierzSaldo()
+    {
+        return saldo;
+    }
+    public void Wyplata(double kwota)
+    {
+        if (saldo >= kwota)
+        {
+            saldo -= kwota;
+            Console.WriteLine($"Wyplacono: {kwota} zł");
+            Console.WriteLine($"Na koncie pozostało: {saldo} zł");
+        }
+        else
+        {
+            Console.WriteLine($"Nie mozna ukonczyc operacji,\n Saldo to {saldo} zł");
+        }
+    }
+}*/
+// Zadanie 6
+/*
+Kot Filemons = new Kot();
+Filemons.Miaucz();
+Filemons.Jedz();
+
+class Zwierze
+{
+    public void Jedz() => Console.WriteLine("Zwierze je");
+}
+
+class Pies : Zwierze
+{
+    public void Szczekaj() => Console.WriteLine("Hau Hau");
+}
+
+class Kot : Zwierze
+{
+    public void Miaucz() => Console.WriteLine("Miau Miau");
+}
+*/
+
+
